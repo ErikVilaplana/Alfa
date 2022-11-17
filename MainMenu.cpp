cd0ecf64599add78607a15d4108023e4a23c619f
ErikVilaplana <erikvilaplana@gmail.com>
2022-11-16 22:56:31 -0300

    Erikito
    Se agrega Menu

---
+++ MainMenu.cpp
@@ -0,0 +1,78 @@
+#include "MainMenu.h"
+
+MainMenu::MainMenu(float width, float height)
+{
+	if (!font.loadFromFile("IMG/galaga.ttf")) {
+		cout << "No font is here";
+	}
+
+
+	//Play
+	mainMenu[0].setFont(font);
+	mainMenu[0].setFillColor(sf::Color::White);
+	mainMenu[0].setString("Jugar");
+	mainMenu[0].setCharacterSize(30);
+	mainMenu[0].setOrigin(mainMenu[0].getGlobalBounds().width / 2, mainMenu[0].getGlobalBounds().height / 2);
+	mainMenu[0].setPosition(710, 200);
+	//Score
+	mainMenu[1].setFont(font);
+	mainMenu[1].setFillColor(sf::Color::White);
+	mainMenu[1].setString("Puntaje");
+	mainMenu[1].setCharacterSize(30);
+	mainMenu[1].setOrigin(mainMenu[1].getGlobalBounds().width / 2, mainMenu[1].getGlobalBounds().height / 2);
+	mainMenu[1].setPosition(710, 300);
+	//About
+	mainMenu[2].setFont(font);
+	mainMenu[2].setFillColor(sf::Color::White);
+	mainMenu[2].setString("Acerca de");
+	mainMenu[2].setCharacterSize(30);
+	mainMenu[2].setOrigin(mainMenu[2].getGlobalBounds().width / 2, mainMenu[2].getGlobalBounds().height / 2);
+	mainMenu[2].setPosition(710, 400);
+	//Exit
+	mainMenu[3].setFont(font);
+	mainMenu[3].setFillColor(sf::Color::White);
+	mainMenu[3].setString("Salir");
+	mainMenu[3].setCharacterSize(30);
+	mainMenu[3].setOrigin(mainMenu[3].getGlobalBounds().width / 2, mainMenu[3].getGlobalBounds().height / 2);
+	mainMenu[3].setPosition(710, 500);
+
+	MainMenuSelected = -1;
+}
+MainMenu::~MainMenu()
+{}
+
+//Draw MainMenu
+
+void MainMenu::draw(RenderWindow& window){
+
+	for (int i = 0; i < Max_main_menu; ++i) {
+		window.draw(mainMenu[i]);
+	}
+}
+
+//MoveUp
+void MainMenu::Moveup() {
+	if (MainMenuSelected - 1 >= 0) {
+		mainMenu[MainMenuSelected].setFillColor(Color::White);
+
+		MainMenuSelected--;
+		if (MainMenuSelected == -1) {
+			MainMenuSelected = 2;
+		}
+		mainMenu[MainMenuSelected].setFillColor(Color::Blue);
+	}
+}
+
+//MoveDown
+void MainMenu::MoveDown() {
+	if (MainMenuSelected + 1 <= Max_main_menu)
+	{
+		mainMenu[MainMenuSelected].setFillColor(Color::White);
+		MainMenuSelected++;
+		if (MainMenuSelected == 4)
+		{
+			MainMenuSelected = 0;
+		}
+		mainMenu[MainMenuSelected].setFillColor(Color::Blue);
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,128 @@
 #include "Juego.h"
+#include "MainMenu.h"
 int main() 
 {
-    Juego juego;
-    juego.init();
+    //INicializar Menu Principal
+
+    RenderWindow MENU(VideoMode(1024, 720), "MAIN MENU", Style::Default);
+    MainMenu mainMenu(MENU.getSize().x, MENU.getSize().y);
+
+    while (MENU.isOpen())
+    {
+        Event event;
+        while (MENU.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+                MENU.close();
+            }
+            if (event.type == Event::KeyReleased)
+            {
+                if (event.key.code == Keyboard::Down)
+                {
+                    mainMenu.MoveDown();
+                    break;
+                }
+                if (event.key.code == Keyboard::Up)
+                {
+                    mainMenu.Moveup();
+                    break;
+                }
+                if (event.key.code == Keyboard::Return) {
+                    RenderWindow Play(VideoMode(1024, 720), "Laraga v2.0");
+                    RenderWindow OPTIONS(VideoMode(1024, 720), "Opciones");
+                    RenderWindow ABOUT(VideoMode(1024, 720), "ACERCA DE");
+
+
+
+                    int x = mainMenu.MainMenuPressed();
+                    if (x == 0)
+                    {
+                        while (Play.isOpen())
+                        {
+                            Event aevent;
+                            while (Play.pollEvent(aevent)) {
+                                if (aevent.type == Event::Closed)
+                                {
+                                    Play.close();
+                                }
+                                if (aevent.type == Event::KeyPressed)
+                                {
+                                    if (aevent.key.code == Keyboard::Escape)
+                                    {
+                                        cout << "poneme aca puto";
+                                        Play.close();
+                                    }
+                                }
+                            }
+                            OPTIONS.close();
+                            ABOUT.close();
+                            Play.clear();
+                            Play.display();
+                        }
+                    }
+                    if (x == 1) {
+                        while (OPTIONS.isOpen())
+                        {
+                            Event aevent;
+                            while (OPTIONS.pollEvent(aevent)) {
+                                if (aevent.type == Event::Closed)
+                                {
+                                    OPTIONS.close();
+                                }
+                                if (aevent.type == Event::KeyPressed)
+                                {
+                                    if (aevent.key.code == Keyboard::Escape)
+                                    {
+                                        OPTIONS.close();
+                                    }
+                                }
+                            }
+                            Play.close();
+                            OPTIONS.close();
+                            ABOUT.close();
+                            OPTIONS.display();
+                        }
+                    }
+                    if (x == 2)
+                    {
+                        while (ABOUT.isOpen())
+                        {
+                            Event aevent;
+                            while (ABOUT.pollEvent(aevent)) {
+                                if (aevent.type == Event::Closed)
+                                {
+                                    ABOUT.close();
+                                }
+                                if (aevent.type == Event::KeyPressed)
+                                {
+                                    if (aevent.key.code == Keyboard::Escape)
+                                    {
+                                        ABOUT.close();
+                                    }
+                                }
+                            }
+                            Play.close();
+                            OPTIONS.clear();
+                            ABOUT.clear();
+                            ABOUT.display();
+                        }
+                    }
+                    if (x == 3)
+                        MENU.close();
+                    break;
+                }
+            }
+        }
+        MENU.clear();
+        mainMenu.draw(MENU);
+        MENU.display();
+    }
+
+
+
+  //  Juego juego;
+    //juego.init();
 }
 
 
